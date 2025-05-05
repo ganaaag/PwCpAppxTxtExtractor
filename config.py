d50a7996d8e3a90cf888b04d0838ce3f215e215d
ganaaag <127759334+ganaaag@users.noreply.github.com>
2025-05-05 20:27:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "25403301"
 api_hash = "d700b4abe3d023165894291b6fcbd3a0"
-bot_token = "7585830486:AAEIfqrVUoX9sSVGXaO0ICVhbQVh7EuOsyw"
+bot_token = "7604457459:AAEKXgqwpKIWNPXDkv-ce1DlCqSdDrERiFs"
 auth_users = [1081310808]
